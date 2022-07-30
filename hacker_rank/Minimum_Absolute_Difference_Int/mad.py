1ac5da833ee8d6eb40871367c2ca4198c6fd3254
David-Chae <aksrud859@hanmail.net>
2022-07-30 19:28:36 +1000

    My python solution for minimum absolute difference problem.

---
+++ hacker_rank/Minimum_Absolute_Difference_Int/mad.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # Write your code here
+    minimum = sys.maxsize
+    arr.sort()
+    i = 0
+    while(i < len(arr)-1):
+        diff = abs(arr[i] - arr[i+1])
+        if diff < minimum : minimum = diff
+        i+=1
+    
+    return minimum
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
